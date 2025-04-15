b06f5d48b737991e04d2f78d5a716309c1352a41
Kish Bogdana <kishbogdana22@gmail.com>
2025-04-15 14:03:55 +0200

    addd .

---
+++ main.js
@@ -0,0 +1,34 @@
+const button = document.getElementById('myButton');
+const input = document.getElementById('textInput');
+
+button.addEventListener('click', () => {
+  button.textContent = input.value;
+});
+
+
+
+
+
+
+const image = document.getElementById('myImage');
+image.src = 'img.png';
+
+
+
+
+
+
+const link = document.getElementById('myLink');
+let image = document.getElementById('linkedImage');
+
+link.href = 'https://app.moiashkola.ua/'; 
+image.alt = 'nenennenenenne';
+
+
+
+const list = document.getElementById('myList');
+const firstItem = list.querySelector('li');
+firstItemer.textContent = 'ennenenenne';
+
+
+
